77a4a1ec04deeb02fb20937dd6f0db1779b66e95
rockclimber147 <daylencsmith@gmail.com>
2026-01-17 20:55:56 -0800

    fix envConfig crash

--- EnvConfig.py
+++ EnvConfig.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class EnvConfig:
-    OPEN_ROUTER_API_KEY = os.getenv("OPEN_ROUTER_API_KEY")
-    AI_ENDPOINT=os.getenv("AI_ENDPOINT")
-    AI_MODEL=os.getenv("AI_MODEL")
+    OPEN_ROUTER_API_KEY = os.getenv("OPEN_ROUTER_API_KEY", None)
+    AI_ENDPOINT=os.getenv("AI_ENDPOINT", None)
+    AI_MODEL=os.getenv("AI_MODEL", None)
